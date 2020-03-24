a5b3018fbc6801d7e78653ecbca73935a3a8208e
aryaman-titan <aryamanbehera@gmail.com>
2020-03-24 21:39:07 +0530

    restarting this series

--- hex2b64.py
+++
@@ -1,6 +0,0 @@
-
-hex_string = raw_input("Enter string : ")
-decoded = hex_string.decode("hex")
-b64_string = decoded.encode("base64")
-
-print(b64_string)
--- s1c4.py
+++
@@ -1,7 +0,0 @@
-#This is a solution to Set-1->Challenge-4
-
-file = open("4.txt" , 'r')
-data = file.read()
-file.close()
-decoded = data.decode("hex")
-print (decoded)
